e8695322e39eadf05dc07916591e92ee9619aee7
blalp <26471229+blalp@users.noreply.github.com>
2021-02-26 04:08:26 -0800

    changed to click event

--- src/main/java/com/blalp/exampleplugin/ExamplePlugin.java
+++ src/main/java/com/blalp/exampleplugin/ExamplePlugin.java
@@ -1,5 +1,6 @@
 package com.blalp.exampleplugin;
 
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ExamplePlugin extends JavaPlugin {
@@ -7,5 +8,14 @@ public class ExamplePlugin extends JavaPlugin {
     public void onEnable() {
         getCommand("examplecommand").setExecutor(new ExampleCommand());
         getServer().getPluginManager().registerEvents(new ItemListener(), this);
+    }
+    @Override
+    public void onDisable() {
+        // When the plugin is disabled make sure no one still has the inventory open, as the listeners that stop them from grabbing items are removed on unload.
+        for(Player player : getServer().getOnlinePlayers()) {
+            if(player.getOpenInventory().getTopInventory().getHolder() instanceof CustomInvetoryHolder) {
+                player.closeInventory();
+            }
+        }
     }
 }
--- src/main/java/com/blalp/exampleplugin/ItemListener.java
+++ src/main/java/com/blalp/exampleplugin/ItemListener.java
@@ -2,22 +2,13 @@ package com.blalp.exampleplugin;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryInteractEvent;
-import org.bukkit.event.inventory.InventoryMoveItemEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
 
 public class ItemListener implements Listener {
     @EventHandler
-    public void onItemInteract(InventoryInteractEvent e) {
+    public void onItemClcik(InventoryClickEvent e) {
         // Since we made all of our inventories with our own CustomInvetoryHolder, we can just check the holder's type to see if the inventory is ours.
-        if (e.getInventory().getHolder() instanceof CustomInvetoryHolder) {
-            e.setCancelled(true);
-            System.out.println("Canceled item movement");
-        }
-    }
-    @EventHandler
-    public void onItemMove(InventoryMoveItemEvent e) {
-        // Since we made all of our inventories with our own CustomInvetoryHolder, we can just check the holder's type to see if the inventory is ours.
-        if (e.getSource().getHolder() instanceof CustomInvetoryHolder) {
+        if (e.getClickedInventory().getHolder() instanceof CustomInvetoryHolder) {
             e.setCancelled(true);
             System.out.println("Canceled item movement");
         }
