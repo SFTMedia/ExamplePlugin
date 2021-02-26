78b2961970bd28aa69051855a835bb19b68a6646
blalp <26471229+blalp@users.noreply.github.com>
2021-02-26 03:52:11 -0800

    Add ItemMoveEvent

--- src/main/java/com/blalp/exampleplugin/ItemListener.java
+++ src/main/java/com/blalp/exampleplugin/ItemListener.java
@@ -3,12 +3,21 @@ package com.blalp.exampleplugin;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryInteractEvent;
+import org.bukkit.event.inventory.InventoryMoveItemEvent;
 
 public class ItemListener implements Listener {
     @EventHandler
     public void onItemInteract(InventoryInteractEvent e) {
         // Since we made all of our inventories with our own CustomInvetoryHolder, we can just check the holder's type to see if the inventory is ours.
         if (e.getInventory().getHolder() instanceof CustomInvetoryHolder) {
+            e.setCancelled(true);
+            System.out.println("Canceled item movement");
+        }
+    }
+    @EventHandler
+    public void onItemMove(InventoryMoveItemEvent e) {
+        // Since we made all of our inventories with our own CustomInvetoryHolder, we can just check the holder's type to see if the inventory is ours.
+        if (e.getSource().getHolder() instanceof CustomInvetoryHolder) {
             e.setCancelled(true);
             System.out.println("Canceled item movement");
         }
